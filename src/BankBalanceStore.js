1defd45b59be783148e211a0f21a7dc8c0133b17
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-05-21 13:21:52 -0500

    With Store from Flux/Utils

--- src/BankBalanceStore.js
+++ src/BankBalanceStore.js
@@ -1,48 +1,41 @@
-import {EventEmitter} from 'fbemitter';
+import {Store} from 'flux/utils';
 import AppDispatcher from './AppDispatcher';
 import constants from './constants';
 //en este archivo se maneja el state
 //se cambia y se devuelve el state
-const CHANGE_EVENT = 'change';
-let __emitter = new EventEmitter();
 let balance = 0;
 
-let BankBalanceStore =
-{
+class BankBalanceStore extends Store {
     getState()
     {
         console.log("va a devolver balance");
         return balance;
-    },
-    addListener: (callback) => {
-        console.log("callback => " + callback);
-        return __emitter.addListener(CHANGE_EVENT, callback);
+    }
+
+    __onDispatch(action)
+    {
+        switch(action.type)
+        {
+            case constants.CREATED_ACCOUNT:
+                balance = 0;
+                this.__emitChange();
+                console.log("en BankStore, caso " + constants.CREATED_ACCOUNT);
+                break;
+            case constants.DEPOSITED_INTO_ACCOUNT:
+                balance = balance + action.ammount;
+                this.__emitChange();
+                console.log("en BankStore, caso " + constants.DEPOSITED_INTO_ACCOUNT);
+                break;
+            case constants.WITHDREW_FROM_ACCOUNT:
+                balance = balance - action.ammount;
+                this.__emitChange();
+                console.log("en BankStore, caso " + constants.WITHDREW_FROM_ACCOUNT);
+                break;
+            default: break;
+
+        }
 
     }
 };
-//aqui se vincula el store con el dispatcher
-//segun el tipo de accion
-BankBalanceStore._dispatchToken = AppDispatcher.register((action) => {
-    switch(action.type)
-    {
-        case constants.CREATED_ACCOUNT:
-            balance = 0;
-            __emitter.emit(CHANGE_EVENT);
-            console.log("en BankStore, caso " + constants.CREATED_ACCOUNT);
-            break;
-        case constants.DEPOSITED_INTO_ACCOUNT:
-            balance = balance + action.ammount;
-            __emitter.emit(CHANGE_EVENT);
-            console.log("en BankStore, caso " + constants.DEPOSITED_INTO_ACCOUNT);
-            break;
-        case constants.WITHDREW_FROM_ACCOUNT:
-            balance = balance - action.ammount;
-            __emitter.emit(CHANGE_EVENT);
-            console.log("en BankStore, caso " + constants.WITHDREW_FROM_ACCOUNT);
-            break;
-        default: break;
 
-    }
-});
-
-export default BankBalanceStore;
+export default new BankBalanceStore(AppDispatcher);
